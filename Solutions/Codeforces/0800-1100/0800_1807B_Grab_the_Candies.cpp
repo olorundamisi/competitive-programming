120557a54c83b30b297fcc871705a8bedbb7b33a
Olorundamisi Dunmade <olorundamisi1@gmail.com>
2023-04-25 22:41:17 +0100

    C++: 1807B - Grab the Candies

---
+++ Solutions/Codeforces/0800-1100/0800_1807B_Grab_the_Candies.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+#define INF 1E9 + 7
+
+using namespace std;
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false), cin.tie(nullptr);
+
+	int64_t T; cin >> T;
+	for (int64_t case_num = 1; case_num <= T; ++case_num) {
+		int64_t n; cin >> n;
+		int64_t x, y; x = y = 0;
+
+		for (int64_t i = 0; i < n; ++i) {
+			int64_t a; cin >> a;
+
+			if (a % 2 == 0) x += a;
+			else y += a;
+		}
+
+		cout << ((x > y) ? "YES\n" : "NO\n");
+	}
+
+
+	return 0;
+}
