43067f36284f40b8aa2248d5dbbc13c9a9813238
Olorundamisi Dunmade <olorundamisi1@gmail.com>
2023-05-06 23:25:41 +0100

    C++: 1822C - Bun Lover

---
+++ Solutions/Codeforces/0800-1100/0800_1822C_Bun_Lover.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+#define INF 1E9 + 7
+
+using namespace std;
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false), cin.tie(nullptr);
+
+	int64_t num_of_test_cases; cin >> num_of_test_cases;
+	for (int64_t case_num = 1; case_num <= num_of_test_cases; ++case_num) {
+		int64_t n; cin >> n;
+
+		cout << 4 * n + 1 + ((n - 1) * (n - 2)) + n - 1 << '\n';
+	}
+
+	return 0;
+}
