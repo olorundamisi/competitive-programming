0aa0a3d245cd1caaecf505385794811aa71af1fa
Olorundamisi Dunmade <olorundamisi1@gmail.com>
2023-06-07 23:02:30 +0100

    C++: 1828A - Divisible Array

---
+++ Solutions/Codeforces/0800-1100/0800_1828A_Divisible_Array.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+#define INF 1E9
+
+using namespace std;
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false), cin.tie(nullptr);
+
+	int64_t num_of_test_cases; cin >> num_of_test_cases;
+	for (int64_t case_num = 1; case_num <= num_of_test_cases; ++case_num) {
+		int64_t n; cin >> n;
+
+		cout << 1 + ((n * (n + 1)) / 2) % n << ' ';
+		for (int64_t i = 2; i <= n; ++i) {
+			cout << i << ' ';
+		}
+		cout << '\n';
+	}
+
+	return 0;
+}
