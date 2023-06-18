210855d36261cdabe190c985f24a064bf73ec358
Olorundamisi Dunmade <olorundamisi1@gmail.com>
2023-06-18 23:04:51 +0100

    C++: 1814A - Game with Board

---
+++ Solutions/Codeforces/0800-1100/0800_1814A_Game_with_Board.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+#define INF 1E9
+
+using namespace std;
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false), cin.tie(nullptr);
+
+	int64_t num_of_test_cases; cin >> num_of_test_cases;
+	for (int64_t case_num = 1; case_num <= num_of_test_cases; ++case_num) {
+		int64_t n; cin >> n;
+
+		if (n > 4) cout << "Alice\n";
+		else cout << "Bob\n";
+	}
+
+	return 0;
+}
