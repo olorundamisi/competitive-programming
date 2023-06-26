a76d8ef68503480af3d2b41b67a5c5acf7e0997c
Olorundamisi Dunmade <olorundamisi1@gmail.com>
2023-06-27 00:18:05 +0100

    C++: 1831A - Twin Permuations

---
+++ Solutions/Codeforces/0800-1100/0800_1831A_Twin_Permutations.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+#define INF 1E9 + 7
+
+using namespace std;
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false), cin.tie(nullptr);
+
+	int64_t num_of_test_cases; cin >> num_of_test_cases;
+	for (int64_t case_num = 1; case_num <= num_of_test_cases; ++case_num) {
+		int64_t n; cin >> n;
+
+		for (int64_t i = 0; i < n; ++i) {
+			int64_t a; cin >> a;
+			cout << n - a + 1 << ' ';
+		}
+		cout << '\n';
+	}
+
+	return 0;
+}
