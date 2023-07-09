398d30d032deddbb52c373d69185df916cfc9ce6
Olorundamisi Dunmade <olorundamisi1@gmail.com>
2023-07-09 23:31:24 +0100

    C++: 1837B - Comparison String

--- Solutions/Codeforces/0800-1100/0800_1837B_Comparison_String.cpp
+++ Solutions/Codeforces/0800-1100/0800_1837B_Comparison_String.cpp
@@ -1,26 +1,27 @@
 #include <bits/stdc++.h>
- 
+
+#define INF 1E9 + 7
+
 using namespace std;
 
-int main()
+int main(void)
 {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    int t;
-    cin >> t;
-    for(int i = 0; i < t; i++)
-    {
-        int n;
-        cin >> n;
-        string s;
-        cin >> s;
-        int ans = 1, cur = 1;
-        for(int i = 1; i < n; i++)
-        {
-            if(s[i] != s[i - 1]) cur = 1;
-            else cur++;
-            ans = max(ans, cur);
-        }
-        cout << ans + 1 << endl;
-    }    
+	ios_base::sync_with_stdio(false), cin.tie(nullptr);
+
+	int64_t test_cases; cin >> test_cases;
+	for (int64_t case_num = 1; case_num <= test_cases; ++case_num) {
+		int64_t n; cin >> n;
+		string s; cin >> s;
+
+		int64_t res = 1, cur = 1;
+		for (int64_t i = 1; i < n; ++i) {
+			if (s[i] == s[i - 1]) ++cur;
+			else cur = 1;
+
+			res = max(res, cur);
+		}
+		cout << res + 1 << '\n';
+	}
+
+	return 0;
 }
